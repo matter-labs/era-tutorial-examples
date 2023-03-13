b7c8989202b86e6ba3873f25436de060a34be09d
Antonio <tonioufa@gmail.com>
2023-03-13 10:18:43 +0000

    updates dependencies

--- local-setup-testing/hardhat.config.ts
+++ local-setup-testing/hardhat.config.ts
@@ -1,29 +1,29 @@
-import { HardhatUserConfig } from 'hardhat/config';
+import { HardhatUserConfig } from "hardhat/config";
 
-require('@matterlabs/hardhat-zksync-deploy');
-require('@matterlabs/hardhat-zksync-solc');
+require("@matterlabs/hardhat-zksync-deploy");
+require("@matterlabs/hardhat-zksync-solc");
 
 // dynamically changes endpoints for local tests
 const zkSyncTestnet =
-  process.env.NODE_ENV == 'test'
+  process.env.NODE_ENV == "test"
     ? {
-        url: 'http://localhost:3050',
-        ethNetwork: 'http://localhost:8545',
+        url: "http://localhost:3050",
+        ethNetwork: "http://localhost:8545",
         zksync: true,
       }
     : {
-        url: 'https://zksync2-testnet.zksync.dev',
-        ethNetwork: 'goerli',
+        url: "https://zksync2-testnet.zksync.dev",
+        ethNetwork: "goerli",
         zksync: true,
       };
 
 const config: HardhatUserConfig = {
   zksolc: {
-    version: '1.2.1',
-    compilerSource: 'binary',
+    version: "1.3.5",
+    compilerSource: "binary",
     settings: {},
   },
-  defaultNetwork: 'zkSyncTestnet',
+  defaultNetwork: "zkSyncTestnet",
   networks: {
     hardhat: {
       // @ts-ignore
@@ -32,7 +32,7 @@ const config: HardhatUserConfig = {
     zkSyncTestnet,
   },
   solidity: {
-    version: '0.8.16',
+    version: "0.8.16",
   },
 };
 
